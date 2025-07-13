db85ed866ec4b57c3b0cceceb68b6f8d768bfbc9
HairulAnamS <hairulanams23@gmail.com>
2025-07-13 19:52:32 +0700

    Update posisi Button

--- tracking.py
+++ tracking.py
@@ -13,11 +13,14 @@ class CameraApp:
         self.video_label = Label(self.window)
         self.video_label.pack(padx=10, pady=10)
 
-        self.btn_start = Button(self.window, text="Open Camera", command=self.start_camera)
-        self.btn_start.pack(side=tk.LEFT, padx=20)
+        self.button_frame = tk.Frame(self.window)
+        self.button_frame.pack(pady=10)
 
-        self.btn_stop = Button(self.window, text="Stop Camera", command=self.stop_camera, state=tk.DISABLED)
-        self.btn_stop.pack(side=tk.RIGHT, padx=20)
+        self.btn_start = Button(self.button_frame, text="Open Camera", command=self.start_camera)
+        self.btn_start.pack(side=tk.LEFT, padx=10)
+
+        self.btn_stop = Button(self.button_frame, text="Stop Camera", command=self.stop_camera, state=tk.DISABLED)
+        self.btn_stop.pack(side=tk.LEFT, padx=10)
 
         self.cap = None
         self.running = False
